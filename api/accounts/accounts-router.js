ae0e9c9427da0acb4ede45a8c2ebb14454f92010
Adam Ingram <kr0nai84@gmail.com>
2024-05-03 00:44:15 -0700

    fixed more failing tests

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,47 +1,56 @@
 const router = require('express').Router()
-const Accounts = require("./accounts-model")
-const {checkAccountId, checkAccountPayload, checkAccountNameUnique} = require("../accounts/accounts-middleware")
+const md = require('./accounts-middleware')
+const Accounts = require('./accounts-model')
+
 
 router.get('/', async (req, res, next) => {
   try {
-    const account = await Accounts.getAll()
-    res.status(200).json(account)
+    const accounts = await Accounts.getAll()
+    res.json(accounts)
   } catch (err) {
-    res.status(500).json({message: err.message})
+    next(err)
   }
-  next()
 })
 
-router.get('/:id',checkAccountId, (req, res, next) => {
-  res.status(200).json(req.account)
-  next()
-})
-
-router.post('/', checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
-  const newAccount = await Accounts.create(req.body)
-  res.status(201).json(newAccount)
-  next()
-})
-
-router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
-  const { id } = req.params
-  const { body } = req.body
-  
-  const updatedAccount = await Accounts.updateById(id, body)
-  res.status(200).json(updatedAccount)
-  next()
+router.get("/:id", md.checkAccountId, (req, res, next) => {
+  Accounts.getById(req.params.id).then((account) => {
+    res.json(account);
+  });
 });
 
-router.delete('/:id', checkAccountId, async (req, res, next) => {
-  const { id } = req.params
-  
-  const deletedAccount = await Accounts.deleteById(id)
-  res.status(200).json(deletedAccount)
-  
-  next()
+router.post('/', md.checkAccountPayload, md.checkAccountNameUnique,
+ async (req, res, next) => {
+  try {
+    const newAccount = await Accounts.create(req.body)
+    res.status(201).json(newAccount)
+  } catch (err){
+    next(err)
+  }
 })
 
-router.use((err, req, res, next) => {
+router.put('/:id', md.checkAccountId, md.checkAccountPayload,
+ md.checkAccountNameUnique, async (req, res, next) => {
+  try {
+    const updated = await Accounts.updateById(req.params.id, req.body)
+    res.status(201).json(updated)
+  } catch (err){
+    next(err)
+  }
+});
+
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
+  try {
+    await Accounts.deleteById(req.params.id)
+    res.json(req.account)
+  } catch {
+    next(err)
+  }
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  // res.status(err.status || 500).json({
+  //   message: err.message
+  // })
 })
 
 module.exports = router;
